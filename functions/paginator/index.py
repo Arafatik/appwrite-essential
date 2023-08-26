50810b7918664edb8258fe3c711af992086ba3a9
Arafat Fayradov <arafatfayradov@gmail.com>
2023-08-26 17:04:54 +0500

    debug

--- functions/paginator/index.py
+++ functions/paginator/index.py
@@ -1,54 +1,21 @@
-from appwrite.client import Client
-
-# You can remove imports of services you don't use
-from appwrite.services.account import Account
-from appwrite.services.avatars import Avatars
-from appwrite.services.databases import Databases
-from appwrite.services.functions import Functions
-from appwrite.services.health import Health
-from appwrite.services.locale import Locale
-from appwrite.services.storage import Storage
-from appwrite.services.teams import Teams
-from appwrite.services.users import Users
-
-"""
-  'req' variable has:
-    'headers' - object with request headers
-    'payload' - request body data as a string
-    'variables' - object with function variables
-
-  'res' variable has:
-    'send(text, status)' - function to return text response. Status code defaults to 200
-    'json(obj, status)' - function to return JSON response. Status code defaults to 200
-
-  If an error is thrown, a response with code 500 will be returned.
-"""
+import random
 
 def main(req, res):
-  client = Client()
+  payload = req.payload or 'No payload provided. Add custom data when executing function.'
 
-  # You can remove services you don't use
-  account = Account(client)
-  avatars = Avatars(client)
-  database = Databases(client)
-  functions = Functions(client)
-  health = Health(client)
-  locale = Locale(client)
-  storage = Storage(client)
-  teams = Teams(client)
-  users = Users(client)
+  secretKey = req.variables.get(
+    'SECRET_KEY',
+    'SECRET_KEY variable not found. You can set it in Function settings.'
+  )
 
-  if not req.variables.get('APPWRITE_FUNCTION_ENDPOINT') or not req.variables.get('APPWRITE_FUNCTION_API_KEY'):
-    print('Environment variables are not set. Function cannot use Appwrite SDK.')
-  else:
-    (
-    client
-      .set_endpoint(req.variables.get('APPWRITE_FUNCTION_ENDPOINT', None))
-      .set_project(req.variables.get('APPWRITE_FUNCTION_PROJECT_ID', None))
-      .set_key(req.variables.get('APPWRITE_FUNCTION_API_KEY', None))
-      .set_self_signed(True)
-    )
-  
+  randomNumber = random.random()
+
+  trigger = req.variables['APPWRITE_FUNCTION_TRIGGER']
+
   return res.json({
-    "areDevelopersAwesome": True,
+    'message': 'Hello from Appwrite!',
+    'payload': payload,
+    'secretKey': secretKey,
+    'randomNumber': randomNumber,
+    'trigger': trigger,
   })
